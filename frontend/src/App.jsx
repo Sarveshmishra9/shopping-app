0e21501d78c3b40ea39e5103c105304ff4913b6e
Dikesh3637 <dikeshreddy.dev@gmail.com>
2025-04-09 21:58:13 +0530

    initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+function App() {
+	return (
+		<>
+			<div className="bg-red-500 text-white text-bold underline">
+				Hello world
+			</div>
+			<Button>Hello there! Click me</Button>
+		</>
+	);
+}
+export default App;
