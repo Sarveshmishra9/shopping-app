4901190ce8b4cec6ee8c06754e7eed1ef6797ffe
Dikesh3637 <dikeshreddy.dev@gmail.com>
2025-04-20 22:35:25 +0530

    added category section with filters

---
+++ frontend/src/pages/category-page.jsx
@@ -0,0 +1,9 @@
+import { SidebarComponent } from "@/components/side-bar-component.jsx";
+
+export default function CategoryPage() {
+    return (
+        <SidebarComponent />
+    )
+}
+
+
---
+++ frontend/src/sections/men-category-section.jsx
@@ -0,0 +1,29 @@
+import dummyData from "../dummy-data/tshirt-dummy-data.json";
+import { ThreeDCard } from "@/components/3d-card-component";
+import { useFilterStore } from "@/store";
+import { filterCategoryData } from "@/lib/utils";
+
+export function MenCategorySection() {
+  const currSetCategories = useFilterStore((state) => state.categories);
+  const currSetSizes = useFilterStore((state) => state.sizes);
+  const currSetThemes = useFilterStore((state) => state.themes);
+  const currSetPriceRange = useFilterStore((state) => state.priceRange);
+
+  const filteredData = dummyData.filter((item) => {
+    return filterCategoryData(
+      item,
+      currSetCategories,
+      currSetSizes,
+      currSetThemes,
+      currSetPriceRange,
+    );
+  });
+
+  return (
+    <div className="w-full men-page grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      {filteredData.map((item) => {
+        return <ThreeDCard key={item.id} {...item} />;
+      })}
+    </div>
+  );
+}
