691ee851bf90ab5af0a35d8d99f6bffa6b8e923d
Mohammad Shan <mohdshan1024@gmail.com>
2025-04-23 23:30:26 +0530

    Userschem update #3

--- zidio-backend/models/User.js
+++ zidio-backend/models/User.js
@@ -23,17 +23,7 @@ const userSchema = new mongoose.Schema({
   },
   avatarUrl: { type: String, default: "" }, // Store avatar URL
 
-  isAdmin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      ref: "Review",
-    },
-  ],
+  
 
   // timestamps: true,
 });
